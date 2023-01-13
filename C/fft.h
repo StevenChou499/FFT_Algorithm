22d0ac1f502633a7ddb0301c33a7f3b4e0e35a5f
StevenChou499 <stevenchou499@gmail.com>
2023-01-14 02:14:04 +0800

    Add some definition for complex calculation

--- C/fft.h
+++ C/fft.h
@@ -1,4 +1,20 @@
 #ifndef FFT
 #define FFT
 
+#include <math.h>
+
+typedef struct complex {
+    double x;
+    double y;
+} compelx;
+
+// conjugate of a complex number
+void conjugate(complex *cpx);
+
+// complex addition
+void complex_add(complex *a, complex *b);
+
+// weighting function
+double W(unsigned r, unsigned N);
+
 #endif
