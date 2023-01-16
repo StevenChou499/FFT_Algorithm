9c0cde92385e8ec14095c1fd80d0e164e153dd3c
StevenChou499 <stevenchou499@gmail.com>
2023-01-17 00:34:19 +0800

    add testing for inverse FFT

--- C/Test_Case.c
+++ C/Test_Case.c
@@ -23,11 +23,11 @@
 
 int main(void)
 {
-    unsigned length = 8U;
+    unsigned length = 4U;
     sequence *what = sequence_init(length);
     for (int i = 0; i < length; i++) {
-        what->arr[i].real = i;
-        what->arr[i].imag = i;
+        what->arr[i].real = i + 1;
+        what->arr[i].imag = i + 1;
     }
     for (int i = 0; i < length; i++) {
         printf("input %d : %f %f\n", i, what->arr[i].real, what->arr[i].imag);
@@ -36,7 +36,12 @@ int main(void)
     for (int i = 0; i < length; i++) {
         printf("output %d : %f %f\n", i, What->arr[i].real, What->arr[i].imag);
     }
+    sequence *whhh = Inverse_FFT(What);
+    for (int i = 0; i < length; i++) {
+        printf("output %d : %f %f\n", i, whhh->arr[i].real, whhh->arr[i].imag);
+    }
     sequence_free(what);
     sequence_free(What);
+    sequence_free(whhh);
     return 0;
 }
