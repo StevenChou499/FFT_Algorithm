22d49f18bceaf43f52c233f281baef3dee9b5cbc
Steven Chou <stevenchou499@gmail.com>
2023-01-20 20:31:44 +0800

    Add test case to 64 inputs

--- C/Test_Case.c
+++ C/Test_Case.c
@@ -2,28 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// int main(void)
-// {
-//     sequence *what = sequence_init(2U);
-//     what->arr[0].real = 1;
-//     what->arr[1].real = 1;
-//     sequence *What = FFT2(what);
-//     printf("%f %f %f %f\n", what->arr[0].real, what->arr[0].imag, what->arr[1].real, what->arr[1].imag);
-//     printf("%f %f %f %f\n", What->arr[0].real, What->arr[0].imag, What->arr[1].real, What->arr[1].imag);
-//     sequence_free(what);
-//     sequence_free(What);
-
-//     int len = 8;
-//     unsigned *list = bit_reverse(len);
-//     for (int i = 0; i < len; i++)
-//         printf("%u\n", list[i]);
-//     free(list);
-//     return 0;
-// }
-
 int main(void)
 {
-    unsigned length = 4U;
+    unsigned length = 64U;
     sequence *what = sequence_init(length);
     for (int i = 0; i < length; i++) {
         what->arr[i].real = i + 1;
